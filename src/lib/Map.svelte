b9b9bcaf757788e4aa9bcdcff121bea6db4e3bb6
Ian Mac Moore <ianmac@blueplusred.com>
2026-03-25 21:30:14 -0700

    First map viewed

---
+++ src/lib/Map.svelte
@@ -0,0 +1,30 @@
+<script>
+	import { onMount } from 'svelte';
+	import mapboxgl from 'mapbox-gl';
+	import 'mapbox-gl/dist/mapbox-gl.css';
+
+	let mapContainer;
+	let map;
+
+	onMount(() => {
+		mapboxgl.accessToken = import.meta.env.VITE_MAPBOX_TOKEN;
+
+		map = new mapboxgl.Map({
+			container: mapContainer,
+			style: 'mapbox://styles/mapbox/dark-v11',
+			center: [-122.6089, 45.5601],
+			zoom: 12
+		});
+
+		return () => map.remove();
+	});
+</script>
+
+<div bind:this={mapContainer} class="map" />
+
+<style>
+	.map {
+		width: 100%;
+		height: 100vh;
+	}
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+	import Map from '$lib/Map.svelte';
+</script>
+
+<h1>irlrpgmap</h1>
+<p>It's a map you discover in real life</p>
+
+<Map />

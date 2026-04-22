586258e424c2f6d92bd945efe79e88a215c8a65d
Ian Mac Moore <ianmac@blueplusred.com>
2026-04-21 22:17:44 -0700

    Install vercel analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,6 +3,11 @@
 	import '../app.css';
 
 	let { children } = $props();
+
+	import { dev } from '$app/environment';
+	import { injectAnalytics } from '@vercel/analytics/sveltekit';
+
+	injectAnalytics({ mode: dev ? 'development' : 'production' });
 </script>
 
 <svelte:head>
